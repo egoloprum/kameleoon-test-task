d4225e338a7d18a296cc29fc7923bb5187d883c5
egoloprum <bayraa093@gmail.com>
2025-11-24 04:50:58 +0300

    features: interval selector and variations selector

--- src/features/intervalSelector/IntervalSelector.tsx
+++ src/features/intervalSelector/IntervalSelector.tsx
@@ -1,3 +1,43 @@
+import { Button } from '@/shared/components/ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/shared/components/ui/popover'
+import { Toggle } from '@/shared/components/ui/toggle'
+import { Square, SquareCheck } from 'lucide-react'
+import { useState } from 'react'
+
 export const IntervalSelector = ({}) => {
-  return <div>IntervalSelector</div>
+  const [selectInterval, setSelectInterval] = useState<'Day' | 'Week'>('Day')
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+
+  const handleIntervalSelect = (interval: 'Day' | 'Week') => {
+    setSelectInterval(interval)
+    setIsPopoverOpen(false) // Close popover on select
+  }
+
+  return (
+    <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+      <PopoverTrigger asChild className="w-20">
+        <Button variant="outline">{selectInterval}</Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-fit flex flex-col">
+        <Toggle
+          pressed={selectInterval === 'Day'}
+          onPressedChange={() => handleIntervalSelect('Day')}
+          aria-label="Interval day"
+          className="data-[state=on]:bg-transparent data-[state=on]:text-foreground justify-start gap-2"
+        >
+          {selectInterval === 'Day' ? <SquareCheck /> : <Square />}
+          Day
+        </Toggle>
+        <Toggle
+          pressed={selectInterval === 'Week'}
+          onPressedChange={() => handleIntervalSelect('Week')}
+          aria-label="Interval week"
+          className="data-[state=on]:bg-transparent data-[state=on]:text-foreground justify-start gap-2"
+        >
+          {selectInterval === 'Week' ? <SquareCheck /> : <Square />}
+          Week
+        </Toggle>
+      </PopoverContent>
+    </Popover>
+  )
 }
--- src/features/variationSelector/VariationSelector.tsx
+++ src/features/variationSelector/VariationSelector.tsx
@@ -1,7 +1,82 @@
-// export const VariationSelector = ({}) => {
-// return <div></div>
-// }
+import { Button } from '@/shared/components/ui/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/shared/components/ui/popover'
+import { ToggleGroup, ToggleGroupItem } from '@/shared/components/ui/toggle-group'
+import { Square, SquareCheck } from 'lucide-react'
+import { useState } from 'react'
 
 export const VariationSelector = ({}) => {
-  return <div>VariationSelector</div>
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false)
+  const [selectedVariations, setSelectedVariations] = useState<string[]>([
+    'Original',
+    'Variation A',
+    'Variation B',
+    'Variation C',
+  ])
+
+  const variations = [
+    {
+      id: 1,
+      value: 'Original',
+    },
+    {
+      id: 2,
+      value: 'Variation A',
+    },
+    {
+      id: 3,
+      value: 'Variation B',
+    },
+    {
+      id: 4,
+      value: 'Variation C',
+    },
+  ]
+
+  const handleVariationChange = (values: string[]) => {
+    // Ensure at least one variation is always selected
+    if (values.length > 0) {
+      setSelectedVariations(values)
+    }
+  }
+
+  const getTriggerText = () => {
+    if (selectedVariations.length === 1) {
+      return selectedVariations[0]
+    } else if (selectedVariations.length === variations.length) {
+      return 'All variations are selected'
+    } else {
+      return `${selectedVariations.length} variations selected`
+    }
+  }
+
+  return (
+    <Popover open={isPopoverOpen} onOpenChange={setIsPopoverOpen}>
+      <PopoverTrigger asChild className="w-50">
+        <Button variant="outline">{getTriggerText()}</Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-fit">
+        <ToggleGroup
+          type="multiple"
+          variant="outline"
+          spacing={2}
+          size="sm"
+          className="flex flex-col"
+          value={selectedVariations}
+          onValueChange={handleVariationChange}
+        >
+          {variations.map(variation => (
+            <ToggleGroupItem
+              key={variation.id}
+              value={variation.value}
+              aria-label={`Select ${variation.value}`}
+              className="data-[state=on]:bg-transparent data-[state=on]:text-foreground w-full justify-start gap-2"
+            >
+              {selectedVariations.includes(variation.value) ? <SquareCheck /> : <Square />}
+              {variation.value}
+            </ToggleGroupItem>
+          ))}
+        </ToggleGroup>
+      </PopoverContent>
+    </Popover>
+  )
 }
